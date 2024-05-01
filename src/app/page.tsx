d816536ee84559376954dc6083564194b1a4bc12
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2024-05-01 06:13:33 -0400

    feat: About section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,20 @@ import About from "@/components/About";
 
 export default function Home() {
   return (
-    <main className="container sm:py-16 py-8 text-neutral-50">
-      <About />
+    <main className="container py-8 text-neutral-50">
+      <About title="Sobre Mim" imagePath="/foto1.png">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a,
+        consequatur in ex laudantium maxime. Esse consequuntur harum asperiores,
+        cum fugit tempora repellendus, minima eveniet, nisi voluptatum dolor
+        facilis assumenda.
+      </About>
+
+      <About imagePath="/foto1.png" link="Ver mais" pathToLink="/" isReverse>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a,
+        consequatur in ex laudantium maxime. Esse consequuntur harum asperiores,
+        cum fugit tempora repellendus, minima eveniet, nisi voluptatum dolor
+        facilis assumenda.
+      </About>
     </main>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,55 @@
-import React from "react";
-import Title from "./Title";
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
 
-function About() {
+interface AboutProps {
+  imagePath: string;
+  isReverse?: boolean;
+  title?: string;
+  link?: string;
+  pathToLink?: string;
+  children: ReactNode;
+}
+
+function About({
+  isReverse,
+  imagePath,
+  title,
+  link,
+  pathToLink,
+  children,
+}: AboutProps) {
   return (
-    <>
-      <Title label="About" id={"about"} />
-    </>
+    <div className="grid grid-cols-2">
+      <div
+        className={`sm:col-span-1 col-span-2 flex items-center justify-center p-6 ${
+          isReverse ? "sm:order-1" : ""
+        }`}
+      >
+        <Image
+          width={368}
+          height={368}
+          src={imagePath}
+          alt="Foto 1 sobre mim."
+          className="rounded-full object-contain border-4 border-primarylighter"
+        />
+      </div>
+      <div className="sm:col-span-1 col-span-2 flex flex-col items-center justify-center p-6">
+        {title && <h1 className="self-start text-3xl py-4">{title}</h1>}
+        <p
+          className={`sm:text-left text-justify ${
+            isReverse ? "sm:text-right" : ""
+          }`}
+        >
+          {children}
+        </p>
+        {link && pathToLink && (
+          <Link href={pathToLink} className="ml-auto py-4 underline">
+            {link}
+          </Link>
+        )}
+      </div>
+    </div>
   );
 }
 
